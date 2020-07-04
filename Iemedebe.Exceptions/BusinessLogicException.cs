1c05dd114925f29e77c42666141b303a1665d83e
Sebastián Bañales <tatantus1@gmail.com>
2020-07-04 04:23:05 -0300

    Created Exceptions project

---
+++ Iemedebe.Exceptions/BusinessLogicException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iemedebe.Exceptions
+{
+    public class BusinessLogicException : Exception
+    {
+        public BusinessLogicException() { }
+        public BusinessLogicException(string message) : base(message) { }
+        public BusinessLogicException(string message, Exception inner) : base(message, inner) { }
+    }
+}
