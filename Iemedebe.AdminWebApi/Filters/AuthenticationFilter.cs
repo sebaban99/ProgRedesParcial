efb147735d70bb46284953539dbfdfc05ef74b70
Sebastián Bañales <tatantus1@gmail.com>
2020-07-04 04:27:37 -0300

    Created Controller + filter + model for logIn/logOut in AdminWebApi

---
+++ Iemedebe.AdminWebApi/Filters/AuthenticationFilter.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Iemedebe.BusinessLogic;
+
+namespace Iemedebe.AdminWebApi
+{
+    public class AuthenticationFilter : Attribute, IAsyncAuthorizationFilter
+    {
+        private ISessionLogic GetSessionLogic(AuthorizationFilterContext context)
+        {
+            var typeOfSessionsLogic = typeof(ISessionLogic);
+            return context.HttpContext.RequestServices.GetService(typeOfSessionsLogic) as ISessionLogic;
+        }
+
+        public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            string token = context.HttpContext.Request.Headers["token"];
+            if (token == null)
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Invalid session token"
+                };
+                return;
+            }
+
+            var sessionLogic = GetSessionLogic(context);
+            
+            if (!await sessionLogic.ValidateSession(new Guid(token)).ConfigureAwait(false))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 403,
+                    Content = "AFValueItemDTO valid session is needed"
+                };
+                return;
+            }
+        }
+    }
+}
